f5d81a93df66347c409cbcf95f402c18f82f953d
Michele Volpato <mvolpato@users.noreply.github.com>
2020-11-15 16:04:01 +0100

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michele Volpato on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
